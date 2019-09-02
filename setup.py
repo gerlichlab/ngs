3fa63d58747c6f56ba1252526c5394d877630f38
Mittmich <michael_mitter@hotmail.com>
2019-09-03 00:03:22 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='NGS',
+    version='0.1',
+    scripts=['HiCTools.py'],
+    author="Michael Mitter",
+    author_email="michael_mitter@hotmail.com",
+    description="Scripts for NGS analysis",
+    long_description="Scripts for NGS analysis",
+    long_description_content_type="",
+    url="https://github.com/gerlichlab/NGS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
